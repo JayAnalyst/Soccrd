3139fea6b91daccffcc8f045a5546ac6ec2c5a0b
JayAnalyst <jamesrowellsp@outlook.com>
2024-12-18 09:51:51 +0000

    words

--- src/words.js
+++ src/words.js
@@ -1,6 +1,9 @@
 // src/words.js
 const words = [
-    "Arsenal", "Chelsea", "Everton", "Liverpool", "Corinthians", "Flamengo", "Palmeiras", "Juventus", "Napoli", "Roma", "Bayern", "Dortmund", "Monaco", "Ajax", "Benfica", "Porto"
-  ];
+   "Arsenal", "Chelsea", "Everton", "Tottenham", "Newcastle", "Leicester", "Bournemouth", "Brighton", "Southampton", "Brentford",
+"Liverpool", "Watford", "Norwich", "Sunderland", "Middlesbrough", "Portsmouth", "Blackburn", "Huddersfield", "Millwall", "Reading",
+"Rangers", "Celtic", "Aberdeen", "Hearts", "Hibernian", "Falkirk", "Motherwell", "Hamilton", "Juventus", "Napoli",
+"Torino", "Bologna", "Atalanta", "Palermo", "Cagliari", "Udinese", "Monaco", "Marseille", "Bordeaux", "Toulouse",
+"Villarreal", "Valencia", "Sevilla", "Granada", "Corinthians", "Fluminense", "Gremio", "Santos", "Fenerbahce", "Galatasaray"  ];
   
   export default words;
